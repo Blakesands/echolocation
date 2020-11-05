b449323649bb7ed4a8199730925be83187879630
Blakesands <71108094+Blakesands@users.noreply.github.com>
2020-11-05 18:09:48 +0000

    Add files via upload

--- test.ino
+++ test.ino
@@ -23,57 +23,17 @@ volatile long int lastsignals[4]; //
 bool havevalues = false;
 //bool clickcall = false;
 
-void comparatortrigger1 ()     
-    { 
-    signals[signalindex] = micros();  /// "micros() works initially but will start behaving erratically after 1-2 ms".?????
-    signalindex++;  //increment the array index to the next position
-    }   // end of ISR
-
-void comparatortrigger2 ()     
-    { 
-    signals[signalindex] = micros();
-    signalindex++;  //increment the array index to the next position 
-    }  // end of ISR
-
-void clickcallisr ()
-    {
-//    clickcall = true;
-    detachInterrupt (digitalPinToInterrupt (3));
-    currentState = ST_PULSEON;  // next state    
-    }
-
-bool wait(unsigned long duration)
-  {
-    static unsigned long startTime;
-    static bool isStarted = false;
-
-    if (isStarted == false)
-    {
-      startTime = micros();
-      isStarted = true;
-      return false;
-    }
-
-    if (micros() - startTime >= duration)
-    {
-      isStarted = false;
-      return true;
-    }
-    
-      return false;
-  }   
-
 AD9833 gen(FNC_PIN);      // Function Generator
 
 void setup() {
 
-    gen.Begin(); 
-    gen.ApplySignal(SINE_WAVE,REG0,10000); // Set pulse
+  gen.Begin(); 
+  gen.ApplySignal(SINE_WAVE,REG0,10000); // Set pulse
 
-    Serial.begin(115200); // debugging
+  Serial.begin(115200); // debugging
 
-    pinMode(2, INPUT); // interrupt pin
-    pinMode(3, INPUT); // interrupt pin
+  pinMode(2, INPUT); // interrupt pin
+  pinMode(3, INPUT); // interrupt pin
 }
 
 void loop()
@@ -81,87 +41,113 @@ void loop()
 
   switch (currentState)
   {
-      case ST_IDLE:
-        // ??? flash some leds?
-        if (havevalues = true) 
-        {
-        Serial.println("Last Scan"); // debugging
-        Serial.println(lastsignals[0]);
-        Serial.println(lastsignals[1]);
-        Serial.println(lastsignals[2]);
-        Serial.println(lastsignals[3]);
-//        Serial.println(lastsignals[4]);
-//        Serial.println(lastsignals[5]);
-        }
-      
-        currentState = ST_CALL;  // next state
-        break;
-        
-      case ST_CALL:
-        attachInterrupt (digitalPinToInterrupt (3), clickcallisr, RISING); // button to start scan moves to next state in isr
-//        while (clickcall = false) 
-//        {
-//          //do nothing
-//        }
-        break;
-        
-      case ST_PULSEON: // switch the PULSE on
-        gen.EnableOutput(true); 
-        signals[signalindex] = micros(); 
-        signalindex++;
-        currentState = ST_WAITON;  // next state
-        break;
-  
-      case ST_WAITON:
-        // check if on pulse duration has lapsed
-        if (wait(20000) == true) // Pulse duration in micros
-        {
-          // if lapsed, go to next step (state)
-          currentState = ST_PULSEOFF;  // next state
-        }
-        break;
-  
-      case ST_PULSEOFF:
-        // switch the PULSE off
-        gen.EnableOutput(false); 
-        signals[signalindex] = micros(); // mark end of signal pulse
-        signalindex++; 
-        currentState = ST_COUNT1; // next state
-        break;
-  
-      case ST_COUNT1:
-        // fill remaining array values
-        attachInterrupt (digitalPinToInterrupt (2), comparatortrigger1, RISING); // increments signal index
-  
-        while (signalindex < 3) {
-          // do nothing
-        }
-        attachInterrupt (digitalPinToInterrupt (2), comparatortrigger2, FALLING);
-  
-        while (signalindex < 4) {
-          // do nothing
-            }
-  
-        if (signalindex == 4)
-            {
-            detachInterrupt (digitalPinToInterrupt (2));
-            havevalues = true;
-            currentState = ST_COUNT2;  // next state 
-            }
-        break;
+    case ST_IDLE:
+    // ??? flash some leds?
+    if (havevalues = true) 
+    {
+      Serial.println("Last Scan"); // debugging
+      Serial.println(lastsignals[0]);
+      Serial.println(lastsignals[1]);
+      Serial.println(lastsignals[2]);
+      Serial.println(lastsignals[3]);
+      //        Serial.println(lastsignals[4]);
+      //        Serial.println(lastsignals[5]);
+    }
 
-      case ST_COUNT2:
-        signalindex = 0; // reset array index count
-        currentState = ST_REPORT;
-        break;
-  
-      case ST_REPORT:
-        for( int i = 0 ; i < 4 ; ++i )
-            {
-            lastsignals[ i ] = signals[ i ]; //flag values - copy to global
-            }
-        currentState = ST_IDLE;
-        break;
-    
+    currentState = ST_CALL;  // next state
+    break;
+
+    case ST_CALL:
+    attachInterrupt (digitalPinToInterrupt (3), clickcallisr, RISING); // button to start scan moves to next state in isr
+    break;
+
+    case ST_PULSEON: // switch the PULSE on
+    gen.EnableOutput(true); 
+    signals[signalindex] = micros(); 
+    signalindex++;
+    currentState = ST_WAITON;  // next state
+    break;
+
+    case ST_WAITON:
+    // check if on pulse duration has lapsed
+    if (wait(20000) == true) // Pulse duration in micros
+    {
+      // if lapsed, go to next step (state)
+      currentState = ST_PULSEOFF;  // next state
+    }
+    break;
+
+    case ST_PULSEOFF:
+    // switch the PULSE off
+    gen.EnableOutput(false); 
+    signals[signalindex] = micros(); // mark end of signal pulse
+    signalindex++; 
+    currentState = ST_COUNT1; // next state
+    break;
+
+    case ST_COUNT1:
+    // fill remaining array values
+    attachInterrupt (digitalPinToInterrupt (2), comparatortrigger1, RISING); // increments signal index
+
+    while (signalindex < 3) {
+      // do nothing
+    }
+    attachInterrupt (digitalPinToInterrupt (2), comparatortrigger2, FALLING);
+
+    while (signalindex < 4) {
+      // do nothing
+    }
+
+    if (signalindex == 4) {
+        detachInterrupt (digitalPinToInterrupt (2));
+        havevalues = true;
+        currentState = ST_COUNT2;  // next state 
+    }
+    break;
+
+    case ST_COUNT2:
+    signalindex = 0; // reset array index count
+    currentState = ST_REPORT;
+    break;
+
+    case ST_REPORT:
+    for( int i = 0 ; i < 4 ; ++i ) {
+      lastsignals[ i ] = signals[ i ]; //flag values - copy to global
+    }
+    currentState = ST_IDLE;
+    break;
+
   }// end of state machine switcher
 } // End of loop
+
+
+void comparatortrigger1 () { 
+  signals[signalindex] = micros();  /// "micros() works initially but will start behaving erratically after 1-2 ms".?????
+  signalindex++;  //increment the array index to the next position
+}   // end of ISR
+
+void comparatortrigger2 () { 
+  signals[signalindex] = micros();
+  signalindex++;  //increment the array index to the next position 
+}  // end of ISR
+
+void clickcallisr () {
+  //    clickcall = true;
+  detachInterrupt (digitalPinToInterrupt (3));
+  currentState = ST_PULSEON;  // next state    
+}
+
+bool wait(unsigned long duration) {
+  static unsigned long startTime;
+  static bool isStarted = false;
+  if (isStarted == false) {
+    startTime = micros();
+    isStarted = true;
+    return false;
+  }
+  if (micros() - startTime >= duration) {
+    isStarted = false;
+    return true;
+  }
+  return false; 
+}   
